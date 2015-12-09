f5219a36144b644b1cc29f5c312c31de9b5b9aac
Kevin Chan <kschan@mit.edu>
2015-12-08 23:39:02 -0500

    Removing commented out code

--- Final_Project2.srcs/sources_1/new/location_heading_calculator.v
+++ Final_Project2.srcs/sources_1/new/location_heading_calculator.v
@@ -100,57 +100,6 @@ module location_heading_calculator(
                 end
             end
         end
-//        case(state)
-//            0: begin
-//                state <= 1;
-//                if (last_x < current_x - 1) begin   //moving right
-
-//                    if (last_com_x1 > last_com_x2)  //com1 leading
-//                        possible_next_x <= current_x+last_com_x1+last_com_x1+last_com_x1+last_com_x1+last_com_x1+last_com_x1-last_com_x2-last_com_x2-last_com_x2-last_com_x2-last_com_x2-last_com_x2;
-//                    else    //com2 leading
-//                        possible_next_x <= current_x+last_com_x2+last_com_x2+last_com_x2+last_com_x2+last_com_x2+last_com_x2-last_com_x1-last_com_x1-last_com_x1-last_com_x1-last_com_x1-last_com_x1;
-                
-//                end else if (last_x > current_x + 1) begin  //moving left
-//                    if (last_com_x1 > last_com_x2)  //com2 leading
-//                        possible_next_x <= current_x+last_com_x2+last_com_x2+last_com_x2+last_com_x2+last_com_x2+last_com_x2-last_com_x1-last_com_x1-last_com_x1-last_com_x1-last_com_x1-last_com_x1;
-//                    else    //com1 leading
-//                        possible_next_x <= current_x+last_com_x1+last_com_x1+last_com_x1+last_com_x1+last_com_x1+last_com_x1-last_com_x2-last_com_x2-last_com_x2-last_com_x2-last_com_x2-last_com_x2;
-                
-//                end else
-//                    possible_next_x <= next_x;
-                
-//                if (last_y < current_y - 1) begin   //moving down
-//                    if (last_com_y1 > last_com_y2)  //com1 leading
-//                        possible_next_y <= current_y+last_com_y1+last_com_y1+last_com_y1+last_com_y1+last_com_y1+last_com_y1-last_com_y2-last_com_y2-last_com_y2-last_com_y2-last_com_y2-last_com_y2;
-//                    else    //com2 leading
-//                        possible_next_y <= current_y+last_com_y2+last_com_y2+last_com_y2+last_com_y2+last_com_y2+last_com_y2-last_com_y1-last_com_y1-last_com_y1-last_com_y1-last_com_y1-last_com_y1;
-                
-//                end else if (last_y > current_y + 1) begin  //moving up
-//                    if (last_com_y1 > last_com_y2)  //com2 leading
-//                        possible_next_y <= current_y+last_com_y2+last_com_y2+last_com_y2+last_com_y2+last_com_y2+last_com_y2-last_com_y1-last_com_y1-last_com_y1-last_com_y1-last_com_y1-last_com_y1;
-//                    else    //com1 leading
-//                        possible_next_y <= current_y+last_com_y1+last_com_y1+last_com_y1+last_com_y1+last_com_y1+last_com_y1-last_com_y2-last_com_y2-last_com_y2-last_com_y2-last_com_y2-last_com_y2;
-                
-//                end else
-//                    possible_next_y <= next_y;
-//            end
-            
-//            1: begin
-//                if(vsync_disp == 0 & last_vsync_disp == 1) begin
-//                    state <= 0;
-//                    last_x <= current_x;
-//                    last_y <= current_y;
-//                    last_predicted_x <= next_x;
-//                    last_predicted_y <= next_y;
-//                end
-                
-//                if ((possible_next_x > (last_predicted_x + 75))|(possible_next_x < (last_predicted_x - 75))|(possible_next_y > (last_predicted_y + 75))|(possible_next_y < (last_predicted_y - 75)))
-//                    next_x <= last_predicted_x;
-//                else
-//                    next_x <= possible_next_x;
-//            end
-//        endcase
-//        end
     end
     
 endmodule
